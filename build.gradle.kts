fae0683bc5a521953e494d0f2e60545ce95e1abd
Ragunath Jawahar <ragunath@redgreen.io>
2021-08-02 21:01:02 +0530

    build: bump up JMH version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
-
 plugins {
   kotlin("jvm") version "1.5.21"
   kotlin("kapt") version "1.5.21"
@@ -16,8 +14,8 @@ dependencies {
   implementation(kotlin("stdlib"))
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
 
-  implementation("org.openjdk.jmh:jmh-core:1.28")
-  kapt("org.openjdk.jmh:jmh-generator-annprocess:1.28")
+  implementation("org.openjdk.jmh:jmh-core:1.32")
+  kapt("org.openjdk.jmh:jmh-generator-annprocess:1.32")
 
   implementation("com.google.truth:truth:1.1.3")
 }
