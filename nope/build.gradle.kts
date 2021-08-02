e1c345055a3862f3f68cbd95bdc2c8e39ee80658
Ragunath Jawahar <ragunath@redgreen.io>
2021-08-03 00:20:26 +0530

    test: add canary test

--- nope/build.gradle.kts
+++ nope/build.gradle.kts
@@ -5,4 +5,14 @@ plugins {
 dependencies {
   implementation(kotlin("stdlib"))
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
+
+  testImplementation(kotlin("test-junit5"))
+  testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+  testImplementation("org.junit.jupiter:junit-jupiter-params:5.7.0")
+  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+  testImplementation("com.google.truth:truth:1.1.2")
+}
+
+tasks.test {
+  useJUnitPlatform()
 }
